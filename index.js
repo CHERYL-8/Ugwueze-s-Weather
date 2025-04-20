5213639e67dfac70e8c63a0783dda397adbbbc5e
CHERYL-8 <ugwuezeobiomacharity@gmail.com>
2025-04-20 23:48:55 +0100

    Added axios added search functionality with API

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+function updateWeather (response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityWeather = document.querySelector("#city-weather");
+    cityWeather.innerHTML = response.data.city;
+    temperatureElement.innerHTML= Math.round(temperature);
+
+}
+function searchCity (city) {
+    let apiKey = c8c71f823b69ab4b8b86o3a0f1d320t8;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+    axios.get(apiUrl).then(updateWeather);
+}
 function handleSearchSubmit(event) {
     event.preventDefault();
     let cityInput = document.querySelector("#city-input");
-    let cityWeather = document.querySelector("#city-weather");
-    cityWeather.innerHTML = cityInput.value;
     
+    searchCity(cityInput.value);
 }
 let searchTabELement = document.querySelector("#search-tab");
 searchTabElement.addEventListener("submit,handleSearchSubmit");
+searchCity("Enugu");
